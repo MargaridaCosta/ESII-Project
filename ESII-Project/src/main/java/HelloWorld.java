be010dd8ee6e3002c30027350f8eb15b4f3b79a0
MargaridaCosta <44871629+MargaridaCosta@users.noreply.github.com>
2020-03-23 21:02:33 +0000

    Com javadoc

--- ESII-Project/src/main/java/HelloWorld.java
+++ ESII-Project/src/main/java/HelloWorld.java
@@ -1,6 +1,12 @@
-
+/**
+ * This is a simple Hello World class
+ */
 public class HelloWorld {
 	
+	/**
+	 * This is the main method that prints "Hello World"
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ ESII-Project/src/test/java/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Classe que testa os métodos da classe HelloWorld
+ */
+public class HelloWorldTest {
+	
+	/**
+	 * Método que testa o main
+	 */
+	@Test
+	public void test() {
+		HelloWorld.main(new String[1]);
+	}
+}
